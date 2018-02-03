750930b26f3ac32026c5c2f2196dce8be50e43b0
Konstantin <phlyze@ya.ru>
2018-02-03 16:36:07 +0200

    Change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,28 @@
 import pkg from './package.json'
 import vue from 'rollup-plugin-vue'
 import buble from 'rollup-plugin-buble'
+import css from 'rollup-plugin-css-only';
 
-const plugins = [
+export default [{
+  input: 'src/main.js',
+  output: {
+    file: pkg.module,
+    format: 'es'
+  },
+  plugins: [
+    vue({ autoStyles: false, styleToImports: true }),
+    css(),
+    buble({ objectAssign: 'Object.assign' })
+  ]
+}, {
+  input: 'src/main.js',
+  output: {
+    file: pkg.main,
+    format: 'umd',
+    name: 'VueTree'
+  },
+  plugins: [
     vue({ css: true }),
     buble({ objectAssign: 'Object.assign' })
-];
-
-export default {
-    input: 'src/main.js',
-    output: [
-        {
-            file: pkg.module,
-            format: 'es'
-        },
-        {
-            file: 'dist/vue-tree.js',
-            format: 'umd',
-            name: 'VueTree'
-        }
-    ],
-    plugins
-}
+  ]
+}];
